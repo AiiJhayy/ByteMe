4436c42c28370f6e2c3579a6c764cb8337225bc5
AJ <angeloredruco456@gmail.com>
2023-05-15 23:24:02 +0800

    Added new pic and php

---
+++ Login_Employer-Seeker.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LOGIN | CreatingPossibility</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="home_style.css" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    </head>
+    <style>
+body {
+    font-family: Arial;
+    color:  white;
+}
+
+.split {
+    height: 100%;
+    width: 50%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.left {
+    left: 0;
+    background-color: #2b6777;
+}
+
+.right {
+    right: 0;
+    background-color: #2b7a78;
+}
+
+.centered {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: white;
+}
+
+.centered img {
+    width: 200px;
+    border-radius: 50%;
+}
+    </style>
+    <body>
+        <a href="RESUME_HTML.html">
+            <div class="split left">
+                <div class="centered">
+                    <img src="Seeker.png" alt="APPLICANT">
+                    <h2>APPLICANT</h2>
+                    <p>Click to login as an Applicant</p>
+                    
+                </div>
+            </div>
+        </a>
+
+        <a href="RESUME.html">
+            <div class="split right">
+                <div class="centered">
+                    <img src="Employer.png" alt="Employer">
+                    <h2>Employer</h2>
+                    <p>Click to login as an Employer</p>
+                    <p>You can post a job offering and accept any applicants</p>
+                </div>
+            </div>
+        </a>
+    </body>
+</html>
