20582c9a99a802ae0e1c5bb6fe99af209a4a54a1
AJ <angeloredruco456@gmail.com>
2023-05-20 22:08:56 +0800

    Database

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@
 $hostName= "localhost";
 $dbUser = "root";
 $dbPassword = "";
-$dbName = "employer-seeker";
+$dbName = "registration_login";
 $connect = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$connect) {
     die("SOMETHING WENT WRONG;");
